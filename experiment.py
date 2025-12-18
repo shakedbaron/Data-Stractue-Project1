c07bdef592e38de1aa437a262ee0f6958d976404
yair-tilayov <yairtilayov@gmail.com>
2025-12-18 13:58:42 +0200

    start of experiment

--- experiment.py
+++ experiment.py
@@ -1,5 +1,7 @@
 import random
 
+from AVLTree import AVLTree
+
 def ordered(i):
     a = []
     for j in range(300*(2**i)):
@@ -28,8 +30,33 @@ def random_close_inversions(i):
             a[k + 1], a[k] = a[k], a[k + 1]
     return a
 
-print(ordered(1)[:20])
-print(opposite_ordered(1)[:20])
-print(random_array(1)[:20])
-print(random_close_inversions(1)[:20])
+#print(ordered(1)[:20])
+#print(opposite_ordered(1)[:20])
+#print(random_array(1)[:20])
+#print(random_close_inversions(1)[:20])
+
+t_ordered = AVLTree()
+ordered = ordered(1)
+total_e = 0
+total_h = 0
+for a in ordered:
+    x, e, h = t_ordered.finger_insert(a, 'a')
+    total_e+=e
+    total_h+=h
+#print(total_e)
+#print(total_h)
+#print(t.avl_to_array())
+
+t_opposite_ordered = AVLTree()
+opposite_ordered = opposite_ordered(1)
+total_e = 0
+total_h = 0
+for a in opposite_ordered:
+    x, e, h = t_opposite_ordered.finger_insert(a, 'a')
+    total_e+=e
+    total_h+=h
+print(total_e)
+print(total_h)
+print(t_opposite_ordered.avl_to_array())
+
 
